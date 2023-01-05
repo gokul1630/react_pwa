5dbb015f735c4656b076b207d486dc93dd7dc576
gokul1630 <gokulgp3167@gmail.com>
2023-01-05 18:41:30 +0530

    Updates

---
+++ sw.js
@@ -0,0 +1,50 @@
+let cacheData = "appV1";
+
+this.addEventListener("install", (event) => {
+  event.waitUntil (
+    caches.open(cacheData).then((cache) => {
+      cache.addAll([
+        "/static/js/bundle.js",
+        "/index.html",
+        "/",
+        "/users",
+        "/about"
+      ])
+    })
+  )
+})
+
+this.addEventListener("fetch", (event) => {
+  if (!navigator.onLine) {
+    event.respondWith(caches.open(cacheData).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        if(cachedResponse) {
+          return cachedResponse;
+        }
+        // let requestUrl = event.request.clone();
+        // return fetch(requestUrl);
+
+        const fetchedResponse = fetch(event.request).then((networkResponse) => {
+          cache.put(event.request, networkResponse.clone());
+
+          return networkResponse;
+        });
+
+        return cachedResponse || fetchedResponse;
+      });
+    }));
+    // event.respondWith(
+    //   caches.match(event.request).then((resp) => {
+    //     if(resp) {
+    //       return resp;
+    //     }
+    //     let requestUrl = event.request.clone();
+    //     return fetch(requestUrl);
+    //   })
+    // )
+  }
+})
+
+this.addEventListener("activate", (event) => {
+  console.log('service worker has been activated.');
+})
